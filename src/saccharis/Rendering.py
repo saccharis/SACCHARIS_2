815422cb2f3de687ef2a1f39eba9e8610d362793
alex <alexander.fraser@alumni.ubc.ca>
2023-08-12 21:14:06 -0700

    fixed rendering not working, also requires rsaccharis 1.0.2

--- src/saccharis/Rendering.py
+++ src/saccharis/Rendering.py
@@ -3,9 +3,15 @@ from logging import getLogger, Logger
 
 
 def render_phylogeny(json_file: str, tree_file: str, output_folder: str, logger: Logger = getLogger()):
-    args = ['Rscript',  f'"library(rsaccharis);C_load_and_plot_all("{json_file}", "{tree_file}", "{output_folder}")"']
+    json_file_double_slash = json_file.replace('\\', '\\\\')
+    tree_file_double_slash = tree_file.replace('\\', '\\\\')
+    output_folder_double_slash = output_folder.replace('\\', '\\\\')
+    load_call = f"C_load_and_plot_all(\'{json_file_double_slash}\', \'{tree_file_double_slash}\', \'{output_folder_double_slash}\')"
+    args = ['Rscript',  '-e', f'"library(rsaccharis)"', '-e',  f'"{load_call}"']
     try:
-        subprocess.run(args, check=True)
+        # the run call doesn't work with args as a list because of weird unquoting behaviour of
+        # subprocess.run() on windows
+        subprocess.run(' '.join(args), check=True)
         logger.info(f"Successfully rendered phylogenetic trees to folder: {output_folder} ")
     except (subprocess.SubprocessError, subprocess.CalledProcessError) as error:
         logger.debug(error)
